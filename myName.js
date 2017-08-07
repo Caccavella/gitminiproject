681959deb69d4a13cd3232600d28077492d6aa94
Anthony <anthony.caccavella@gmail.com>
2017-08-07 13:59:03 -0600

    Added psuedo code to make it interesting

--- myName.js
+++ myName.js
@@ -1,2 +1,5 @@
 Anthony Caccavella
 //This is a change that I have made in order to fulfill the mini project//
+function newName(var) {
+    //this is where you could create code that could give you new functions//
+}
